5f8153cfa6085050197c8bc2ad3c3eb0fc87c883
ANANTH4004 <balamuruganananth@gmail.com>
2025-05-05 08:27:54 +0530

    promise is eagarly loading

--- src/app/rxjs/rxjs.component.ts
+++ src/app/rxjs/rxjs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-rxjs',
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './rxjs.component.html',
   styleUrl: './rxjs.component.scss',
 })
-export class RxjsComponent {}
+export class RxjsComponent implements OnInit {
+  ngOnInit(): void {
+    const promise = new Promise((resolve, reject) => {
+      console.log('Promise started!');
+      resolve('Promise resolved!');
+    });
+
+    promise.then((res) => console.log(res));
+    console.log('Promise finished!');
+  }
+}
